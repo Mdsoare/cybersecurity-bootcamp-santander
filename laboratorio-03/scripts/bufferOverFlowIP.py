7f9d1f747d34740f1dfa1b2d8f0e9fc90abb285d
Marcelo Soares <106860930+Mdsoare@users.noreply.github.com>
2024-10-04 10:02:45 -0300

    Doc: Inser remediação no código

--- laboratorio-03/scripts/bufferOverFlowIP.py
+++ laboratorio-03/scripts/bufferOverFlowIP.py
@@ -20,3 +20,26 @@ def parse_ip(ip_input):
 ip_input = input("Digite um endereço IP: ")
 result = parse_ip(ip_input)
 print(result)
+
+'''
+# REMEDIAÇÃO: Para remediar essa vulnerabilidade, podemos adicionar uma verificação do comprimento da string antes de passá-la para a função 'ipaddress.ip_address'
+
+import ipaddress
+
+MAX_IP_LENGTH = 15  # Limite máximo para o comprimento de um endereço IPv4
+
+def parse_ip(ip_input):
+    if len(ip_input) > MAX_IP_LENGTH:
+        return "Endereço IP muito longo"
+
+    try:
+        ip = ipaddress.ip_address(ip_input)
+        return f"Endereço IP válido: {ip}"
+    except ValueError:
+        return "Endereço IP inválido"
+
+ip_input = input("Digite um endereço IP: ")
+result = parse_ip(ip_input)
+print(result)
+
+'''
